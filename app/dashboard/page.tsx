a6d681e3a59eee531bc22dfe6a81ac20a607e8fd
domin <dominik.albiniak1@gmail.com>
2024-06-09 20:07:09 +0200

    publish RevenueChart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,11 @@ import {Card} from '@/app/ui/dashboard/cards'
 import RevenueChart from '../ui/dashboard/revenue-chart';
 import LatestInvoices from '../ui/dashboard/latest-invoices';
 import { lusitana } from '../ui/fonts';
-import { fetchRevenue } from '../lib/data';
+import { fetchRevenue, fetchLatestInvoices } from '../lib/data';
 
 export default async function Page() {
     const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2x1`}>
